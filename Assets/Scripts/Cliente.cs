bbb97a3a884bddcf83b4477f5ce36b4b27a3e049
Bardecho00 <bardecho00@gmail.com>
2025-02-04 19:32:56 +0100

    clientes

---
+++ Assets/Scripts/Cliente.cs
@@ -0,0 +1,65 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Cliente : MonoBehaviour
+{
+    [SerializeField] Spawn_Clientes sp;
+
+    [SerializeField] GameObject[] Texst = new GameObject[3] ;
+
+    GameObject x;
+
+    int N_plato;
+    void Start()
+    {
+        N_plato = Random.Range(1, 4);
+
+
+        if (N_plato == 1)
+        {
+            gameObject.tag = "Plato_1";
+            
+             x = Instantiate(Texst[0], gameObject.transform.position + new Vector3(1, 1, 0), Quaternion.Euler(0, 0, 0));
+
+
+        } else if (N_plato == 2) 
+        {
+            gameObject.tag = "Plato_2";
+            x = Instantiate(Texst[1], gameObject.transform.position + new Vector3(1, 1, 0), Quaternion.Euler(0, 0, 0));
+        }
+        else if (N_plato == 3) 
+        {
+            gameObject.tag = "Plato_3";
+            x = Instantiate(Texst[2], gameObject.transform.position + new Vector3(1, 1, 0), Quaternion.Euler(0, 0, 0));
+        }
+        
+
+    }
+
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == gameObject.tag) {
+
+            sp.h();
+
+            s();
+
+            Destroy(x);
+        
+        }
+    }
+
+    public void s()
+    {
+
+        gameObject.tag = "ella me dice ola";
+
+
+    }
+    void Update()
+    {
+      
+    }
+}
--- Assets/Scripts/Spawn_Clientes.cs
+++ Assets/Scripts/Spawn_Clientes.cs
@@ -10,48 +10,38 @@ public class Spawn_Clientes : MonoBehaviour
 
     public int R;
 
-    public bool ocupado = false;
+    
     void Start()
     {
-        StartCoroutine(spawn_cliente());
-    }
-
-    IEnumerator spawn_cliente()
-    {
-        R = Random.Range(0,cliente.Length);
-
-        Instantiate(cliente[0],silla.transform);
-
-        yield return new WaitForSeconds(2f);
-
-        ocupado = true;
-    }
-
-    IEnumerator receta()
-    {
-        print("Hola");
-
-        yield return new WaitForSeconds(5f);
-
-        StartCoroutine(spawn_cliente());
-
-
         
     }
+
+    public void h() {
+
+        StartCoroutine(spawn_cliente());
+
+         IEnumerator spawn_cliente()
+        {
+            R = Random.Range(0, cliente.Length);
+
+            yield return new WaitForSeconds(2f);
+
+            Instantiate(cliente[0], silla.transform.position, Quaternion.Euler(0, 0, 0));
+
+            yield return new WaitForSeconds(2f);
+
+
+        }
+    }
+    
+
+ 
     
     void Update()
     {
        
 
-        if (Input.GetKeyUp(KeyCode.E) && ocupado) {
-
-            ocupado = false;
-
-            StartCoroutine(receta());
-        
-        
-        
-        }
+       
 
     }
 }
